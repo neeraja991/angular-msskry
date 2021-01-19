5da2e87a6682bc87abaf13a10cae49a973bbe2a5
neeraja991 <neerajaganesh@gmail.com>
2021-01-19 16:08:41 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,6 +31,35 @@ export class AppComponent  {
     }
   ];
 
+  people1: any[] = [
+    {
+      "name": "Douglas  Pace",
+      "age": 35,
+      "country": 'MARS'
+    },
+    {
+      "name": "Mcleod  Mueller",
+      "age": 32,
+      "country": 'USA'
+    },
+    {
+      "name": "Day  Meyers",
+      "age": 21,
+      "country": 'HK'
+    },
+{
+      "name": "Aguirre  Ellis",
+      "age": 34,
+      "country": 'UK'
+    },
+    {
+      "name": "Cook  Tyson",
+      "age": 32,
+      "country": 'USA'
+    }
+  ];
+ 
+
   peopleByCountry: any[] = [
     {
       'country': 'UK',
